75121a66e23bcec929298ca1e7f05b397eed227f
patronlaurel <mayerlau@students.zhaw.ch>
2025-01-06 22:39:58 +0100

    final changes for netlify

--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,9 @@
 import adapter from '@sveltejs/adapter-netlify';
-
 export default {
-    kit: {
-        adapter: adapter({
-            edge: false,
-            split: false
-        }),
-        // Add static asset handling
-        files: {
-            assets: 'static'
-        }
-    }
+	kit: {
+		adapter: adapter({
+			edge: false,
+			split: false
+		})
+	}
 };
