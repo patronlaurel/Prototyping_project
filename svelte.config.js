49bc3842e9a65b65d29350fcad053ba505caa6d0
patronlaurel <mayerlau@students.zhaw.ch>
2025-01-06 22:27:59 +0100

    Netlify changes

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,24 @@
-import adapter from '@sveltejs/adapter-auto';
+// import adapter from '@sveltejs/adapter-netlify';
+// export default {
+// 	kit: {
+// 		adapter: adapter({
+// 			edge: false,
+// 			split: false
+// 		})
+// 	}
+// };
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
-	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
-	}
+import adapter from '@sveltejs/adapter-netlify';
+
+export default {
+    kit: {
+        adapter: adapter({
+            edge: false,
+            split: false
+        }),
+        // Add static asset handling
+        files: {
+            assets: 'static'
+        }
+    }
 };
-
-export default config;
